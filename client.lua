8f517b4abdd9244b8e3542d2714fffd0a9feebff
ElevateStudios771 <anurmesic@gmail.com>
2025-03-11 08:46:06 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,13 +2,13 @@
 local blindfoldActive = false
 
 RegisterCommand("blindfold", function()
-    blindfoldActive = not blindfoldActive -- Toggle blindfold state
+    blindfoldActive = not blindfoldActive 
     
     if blindfoldActive then
-        SendNUIMessage({ action = "show" }) -- Activate NUI black screen
+        SendNUIMessage({ action = "show" }) 
         SetNuiFocus(true, true)
     else
-        SendNUIMessage({ action = "hide" }) -- Remove NUI black screen
+        SendNUIMessage({ action = "hide" })
         SetNuiFocus(false, false)
     end
 end, false)
@@ -23,7 +23,7 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         if blindfoldActive then
-            DrawRect(0.5, 0.5, 1.0, 1.0, 0, 0, 0, 255) -- Fallback if NUI fails
+            DrawRect(0.5, 0.5, 1.0, 1.0, 0, 0, 0, 255) 
         end
     end
 end)
